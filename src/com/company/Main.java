6ae4a137f432f7a04911c62a9852407fc555bb05
piotrs <piotrek.priv@gmail.com>
2020-07-09 21:05:17 +0200

    added user input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World");
 
+        Scanner input = new Scanner(System.in);
+
+        int l1 = input.nextInt();
+        int l2 = input.nextInt();
+
+        System.out.println("l1*l2");
     }
 }
